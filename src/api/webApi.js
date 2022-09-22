80cd598714bfdb0087fbe41c4f4196d511fda188
VTrofym <v.trof@icloud.com>
2022-09-22 17:20:41 +0300

    finish hw 11

--- src/api/webApi.js
+++ src/api/webApi.js
@@ -1,23 +1,27 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import { itemPerPage } from '../index';
 
-const API_KEY = '30101900-ea341d140d323a7caf8313ccb';
+export const itemPerPage = 40;
+
+const API_KEY = '30087713-0176780a5d439b6fbf7b0e04e';
 const searchParams = new URLSearchParams({
   key: API_KEY,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
+  per_page: itemPerPage,
 });
 
 export const BASE_URL = `https://pixabay.com/api/?${searchParams}`;
 
-export async function getPhoto(search) {
+export async function getPhoto(search, page) {
   try {
     if (!search.trim()) {
       console.log('no arg!');
       return;
     }
-    const response = await axios.get(`${BASE_URL}&q=${search}`);
+    const response = await axios.get(`${BASE_URL}&page=${page}&q=${search}`);
     return response.data;
   } catch (error) {
     Notiflix.Notify.failure(error.message);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 import './css/styles.css';
-import { BASE_URL, getPhoto } from './api/webApi';
+import { BASE_URL, getPhoto, itemPerPage } from './api/webApi';
 import Notiflix from 'notiflix';
 
 const galleryEl = document.querySelector('.gallery');
 const formEl = document.querySelector('#search-form');
+const moreBtn = document.querySelector('.load-more');
+
+let page = 1;
+
+const totalPages = Math.ceil(500 / itemPerPage);
 
 formEl.addEventListener('submit', onSubmit);
 
+async function loadMoreCards(searchValue) {
+  page += 1;
+  const data = await getPhoto(searchValue, page);
+  data.hits.forEach(photo => {
+    createCardMarkup(photo);
+  });
+  if (page === totalPages) {
+    moreBtn.classList.add('visually-hidden');
+  }
+}
+
 function onSubmit(event) {
   event.preventDefault();
+
+  clearMarkup(galleryEl);
 
   const searchValue = event.currentTarget[0].value;
   mountData(searchValue);
@@ -16,7 +34,12 @@ function onSubmit(event) {
 
 async function mountData(searchValue) {
   try {
-    const data = await getPhoto(searchValue);
+    const data = await getPhoto(searchValue, page);
+
+    moreBtn.classList.remove('visually-hidden');
+    moreBtn.addEventListener('click', () => {
+      loadMoreCards(searchValue);
+    });
     if (data.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -59,4 +82,8 @@ function createCardMarkup({
   </div>
 </div>`
   );
+}
+
+function clearMarkup(element) {
+  element.innerHTML = '';
 }
